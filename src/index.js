8f56a53a92e860b18afb0793f18f36136d16fd50
Alex Bepple <alex@bepple.de>
2017-04-23 20:29:30 +0200

    use Redux Devtools, if available

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 
 import rootReducer from './state'
 import App from './components/App'
 
-const store = createStore(rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(
+    // add middlewares here
+  ))
+)
 
 render(
   <Provider store={store}>
